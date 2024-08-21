74eef201fe2b382bef03fb825206948a84f4b208
sadekul <setubd25@gmail.com>
2024-08-21 15:57:24 +0600

    user-react-icon

--- src/components/use-react-icon.js
+++ src/components/use-react-icon.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { TbCoffee } from "react-icons/tb";
 import { GiCoffeeCup } from "react-icons/gi";
-
+//how to use react icons
 export default function Icon() {
   return (
     <div>
+        
         return <h3> Lets go for a <TbCoffee className='icon' />or <GiCoffeeCup  className='icon'/> </h3>
     </div>
   )
