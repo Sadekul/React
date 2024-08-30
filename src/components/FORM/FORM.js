1bf62d9251c461daca412dd6858839070c0a7e01
sadekul <setubd25@gmail.com>
2024-08-30 16:21:04 +0600

    useSatate-with-Object

--- src/components/FORM/FORM.js
+++ src/components/FORM/FORM.js
@@ -3,58 +3,40 @@ import Style from './form.module.css'
 
 export default function FORM() {
 
-const [name, setName] = useState('');
-const [email, setEmail] = useState('');
-const [passward, setPassword] = useState('');
+const [user, setUser] = useState({name: '', email: '', passward: ''})
 
-const handleNameChange = (e) =>{
-   setName(e.target.value);
-}
+//destructuring
+const {name, email, passward} = user
 
-const handleEmailChange = (e) =>{
-    setEmail(e.target.value);
-}
+//event handeler
+const handleChange = (e) => {
+  // ...user this is spread operator ... 3dots
+  setUser({...user, [e.target.name]: e.target.value});
+};
 
-const handlePasswardChange = (e) =>{
-   setPassword(e.target.value)
-}
 const handleSubmit = (e) =>{
-    console.log("form is submitted");
+   console.log("form is submitted");
 
-   let userInfo = {
-//property and value same thakle dui bar likher dorkar nai
-// Ex: name: name, = name,
-
-    // name : name,
-    // email : email,
-    // passward : passward
-        name,
-        email,
-        passward
-   }
-
-   console.log(userInfo);
+   console.log(user);
 
     //this line for page not reload or refresh autometically
     e.preventDefault();
 }
-
-
   return (
     <div>
      <h2> Registration</h2>
      <form action='' onSubmit={handleSubmit}>
        <div className={Style.fromGroup}>
             <label htmlFor="name">Name: </label>
-            <input type='text' name='name' id='name' value={name} onChange={handleNameChange} required />
+            <input type='text' name='name' id='name' value={name} onChange={handleChange} required />
        </div>
         <div className={Style.fromGroup}>
              <label htmlFor="email">Email: </label>
-            <input type='email' name='email' id='email' value={email} onChange={handleEmailChange} required />
+            <input type='email' name='email' id='email' value={email} onChange={handleChange} required />
         </div>
         <div className={Style.fromGroup}>
              <label htmlFor="passward">Passward: </label>
-            <input type='passward' name='passward' id='passward' value={passward} onChange={handlePasswardChange} required />
+            <input type='passward' name='passward' id='passward' value={passward} onChange={handleChange} required />
         </div>
         <div>
             <button type='submit'>Register</button>
